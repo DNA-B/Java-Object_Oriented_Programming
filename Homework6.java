5c19aceac22a8bdf20734aac7f3b1751bf8b5b70
변성훈 <sphz3813@gmail.com>
2023-11-13 20:26:21 +0900

    Add files via upload

---
+++ Homework6.java
@@ -0,0 +1,53 @@
+class Course {
+    private String code;
+    private String name;
+
+    public Course(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Code: " + code + ", Name: " + name;
+    }
+}
+
+class OnlineCourse extends Course {
+    public OnlineCourse(String code, String name) {
+        super(code, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Code: " + super.getCode() + ", Name: " + super.getName() + ", Type: Online";
+    }
+}
+
+class OfflineCourse extends Course {
+    public OfflineCourse(String code, String name) {
+        super(code, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Code: " + super.getCode() + ", Name: " + super.getName() + ", Type: Offline";
+    }
+}
+
+public class Homework6 {
+    public static void main(String[] args) {
+        OfflineCourse course1 = new OfflineCourse("HAEA9201", "Object Oriented Programming");
+        OnlineCourse course2 = new OnlineCourse("HALF0012", "C Programming 1");
+        System.out.println(course1);
+        System.out.println(course2);
+    }
+}
